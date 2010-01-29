4ec6c8f874383aba2db1e5b6fe8cea326dc3ae97
Justin Azoff <justin@bouncybouncy.net>
2010-01-29 15:23:38 -0500

    added setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from glob import glob
+
+setup(name="wirelessprobe",
+    version="1.0",
+    author="Justin Azoff",
+    author_email="JAzoff@uamail.albany.edu",
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    entry_points = {
+        'console_scripts': [
+        ]
+    },
+    scripts=glob('scripts/*'),
+    setup_requires=[
+        "nose",
+    ],
+    test_suite='nose.collector',
+)
